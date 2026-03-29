ee88cbdf3b8aff2072989827a3130e56b9090d6c
Your Name <you@example.com>
2026-03-30 01:27:40 +0800

    Fix Windows installed app data path

--- src/app/common/app-props.js
+++ src/app/common/app-props.js
@@ -2,11 +2,12 @@
  * app path
  */
 const { app } = require('electron')
-const { resolve } = require('path')
+const { resolve, dirname } = require('path')
 const constants = require('./runtime-constants')
 const installSrc = require('../lib/install-src')
 
 function getDataPath () {
+  const appName = app.getName() || 'electerm'
   const defaultValue = {
     appPath: app.getPath('appData'),
     isPortable: false
@@ -14,8 +15,8 @@ function getDataPath () {
   if (!constants.isWin) {
     return defaultValue
   }
-  const exePath = app.getPath('exe').replace('\\electerm.exe', '')
-  const p = exePath + '\\' + 'electerm'
+  const exePath = dirname(app.getPath('exe'))
+  const p = resolve(exePath, appName)
   if (
     installSrc === 'win-x64-portable.tar.gz' ||
     require('fs').existsSync(
